5b51a84b0629a8185a07a37c007e9410fbb82e42
Devanshu <devgoyal952@gmail.com>
2024-12-19 17:40:29 +0530

    developed FetchingData component

--- src/FetchingData.js
+++ src/FetchingData.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FetchingData = () => {
-  return <div></div>;
+  const [data, setData] = useState();
+
+  const fetchingData = async () => {
+    try {
+      const data = await fetch("https://fakestoreapi.com/products");
+      const response = await data.json();
+      setData(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  return data ? <h1>We have got the</h1> : <h1> We have not got the data </h1>;
 };
 
 export default FetchingData;
