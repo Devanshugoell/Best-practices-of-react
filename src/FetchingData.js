c3cd0ec64d28ab3d11f3d12ca3cb76b97ae07984
Devanshu <devgoyal952@gmail.com>
2024-12-19 17:47:45 +0530

    displayed data on the web page

--- src/FetchingData.js
+++ src/FetchingData.js
@@ -8,6 +8,7 @@ const FetchingData = () => {
       const data = await fetch("https://fakestoreapi.com/products");
       const response = await data.json();
       setData(response);
+      console.log(response);
     } catch (error) {
       console.log(error);
     }
@@ -17,7 +18,22 @@ const FetchingData = () => {
     fetchingData();
   }, []);
 
-  return data ? <h1>We have got the</h1> : <h1> We have not got the data </h1>;
+  return data ? (
+    data.map((item) => {
+      return (
+        <div key={item.id} className="products">
+          <h1>{item.title}</h1>
+          <p>{item.category}</p>
+          <img src={item.image} alt="product" />
+          <p>{item.description}</p>
+          <p>{item.rating.count}</p>
+          <p>{item.price} $</p>
+        </div>
+      );
+    })
+  ) : (
+    <h1> We have not got the data </h1>
+  );
 };
 
 export default FetchingData;
